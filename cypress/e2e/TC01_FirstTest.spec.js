8a5872c68f6480965a80adc864109e77c8176712
Phoenix Shed-Okara <nixokara@gmail.com>
2024-03-24 16:35:58 +0100

    First Push

---
+++ cypress/e2e/TC01_FirstTest.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe ('Login Functionality' , function(){
+
+    it('Login Test using Conduit' , function(){
+        cy.visit('https://react-redux.realworld.io/#/?_k=6s6l5p')
+        //cy.get(':nth-child(2) > .nav-link').click()
+    })
+})
