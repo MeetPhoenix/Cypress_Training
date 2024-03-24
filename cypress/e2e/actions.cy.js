ac3b4a69dd395a7dd60fa352788765ea4c232809
Phoenix Shed-Okara <nixokara@gmail.com>
2024-03-24 16:40:37 +0100

    LoginTest

--- cypress/e2e/actions.cy.js
+++ cypress/e2e/actions.cy.js
@@ -10,6 +10,9 @@ context('Actions', () => {
    it('.type() - type into a DOM element', () => {
     cy.visit('https://react-redux.realworld.io/#/?_k=6s6l5p')
     cy.get(':nth-child(2) > .nav-link').click()
+    cy.get(':nth-child(1) > .form-control').type('cypressdemo@gmail.com')
+    cy.get(':nth-child(2) > .form-control').type('cypressdemo')
+    cy.get('.btn').click()
   //   // https://on.cypress.io/type
   //   cy.get('.action-email').type('fake@email.com')
   //   cy.get('.action-email').should('have.value', 'fake@email.com')
